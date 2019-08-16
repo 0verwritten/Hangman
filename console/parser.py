240414d04d3011c840cd44717a653c7826fc362a
TheSimpleWriter <simplepcwriter@gmail.com>
2019-08-16 10:49:21 +0300

    make class to get random word and make little update for Dictionary class

--- console/parser.py
+++ console/parser.py
@@ -3,7 +3,6 @@ import requests
 class Dictionary(): ## class for get description of word
 	def __init__(self, word):
 		if word == "":
-			print("Invaild word")
 			return "Invaild word"
 		self.word  = word
 		data = self.__get_data()
@@ -11,18 +10,17 @@ class Dictionary(): ## class for get description of word
 			return f"The word you've entered isn't in the dictionary."
 		return data
 
-	def find(self,val, query, next_val=0, start=0):
+	def _find(self,val, query, next_val=0, start=0):
 		for x in range(start, len(query)-len(val)+1):
 			if query[x:(x+len(val))] == val:
 				if next_val > 0:
 					next_val -= 1
 					continue
 				return x
-		print('no')
-	def get_values(self,start, end, query):
-			start_index = self.find(start, query)
-			end_index = self.find(end, query, start=start_index) + len(end)
-			print(f"text is: '{query[start_index:end_index]}'")
+		return False
+	def _get_value(self,start, end, query, delay=0):
+			start_index = self._find(start, query)
+			end_index = self._find(end, query, start=start_index, next_val=delay) + len(end)
 			return [query[start_index:end_index],end_index] ## second object is to make right query cut
 
 
@@ -31,7 +29,7 @@ class Dictionary(): ## class for get description of word
 		if f"The word you\'ve entered isn\'t in the dictionary." in request.text:
 			return False
 		while True:
-			res, i = self.get_values('<span class="dtText"><strong class="mw_t_bc">', '</span>', request.text)
+			res, i = self._get_value('<span class="dtText"><strong class="mw_t_bc">', '</span>', request.text)
 			request = request.text[i:]
 			if not self.word[:3] in res:
 				res = res.replace('<', "`#")
@@ -39,8 +37,27 @@ class Dictionary(): ## class for get description of word
 				res = res.split("`")
 				for x in range(len(res)-1, -1, -1):
 					if '#' in res[x] or res[x] == '' or res[x] == ": ":
-						print(res[x])
 						del res[x]
 				res = ''.join(res) # end value
 				return res
+
+class RandomWord(Dictionary): ## class to get random word
+	def __init__(self):
+		self.rand_word, self.description = self.__get_data()
+		print(self.rand_word)
+
+
+
+	def __get_data(self):
+		request = requests.get('https://randomword.com/')
+		res, i = super()._get_value('<div id="shared_section">', '</div>', request.text, delay=2)
+		request = request.text[i:]
+		res = res.replace('<', "`#")
+		res = res.replace('>', "`")
+		res = res.split("`")
+		for x in range(len(res)-1, -1, -1):
+			if '#' in res[x] or res[x] == '' or '\n\t\t' in res[x]:
+				del res[x]
+		res[1] = res[1][:len(res[1])-2]
+		return res# end value
 
