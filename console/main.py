15d8162dc7cd49dc956283ec38c2b88448f0faf5
TheSimpleWriter <simplepcwriter@gmail.com>
2019-08-16 12:22:42 +0300

    make final version of console hangman game

--- console/main.py
+++ console/main.py
@@ -36,5 +36,59 @@ if ans == 2:
 		word = f[0].split('||')
 		word = {'word': word[0], 'description': word[1]}
 
-print(word)
+attempt_count=''
+while True:
+	try:
+		attempt_count = int(input("How many attempt do you want? from 0 to 16 (0 = infinity count) "))
+		if attempt_count >= 0 and attempt_count <=16:
+			break
+	except ValueError:
+		print("Enter number from 0 to 16")
 
+answered = ['*' for x in word['word']]
+all_prev = []
+prev = ''
+right_att = True ## variable to check is last attempt is right
+repeated_att = False ## variable to check is last attempt is repeted
+
+while attempt_count != -1:
+	print()
+	print("Word: "+ ''.join(answered))
+	print("Description of word: "+word['description'])
+	print("Previous guess: "+ prev)
+	print("Attempt left: " + ("infinity" if attempt_count == 0 else str(attempt_count)))
+	if not right_att:
+		print("In this word no letter: " + prev)
+	if repeated_att:
+		print("This letter you have already entered")
+	ans = input("Choose the letter: ")
+	for x in all_prev:
+		if x == ans:
+			repeated_att = True
+			os.system('clear')
+			continue
+	right_att = False
+	for x in range(0,len(word['word'])):
+		if ans == word['word'][x]:
+			answered[x] = ans
+			print(''.join(answered),word['word'])
+			print(''.join(answered) == word['word'])
+			right_att = True
+	os.system('clear')
+	if ''.join(answered) == word['word']:
+		break
+	if attempt_count != 0 and not right_att:
+		attempt_count -= 1
+		if attempt_count == 0:
+			attempt_count = -1
+	prev = ans
+	all_prev.append(prev)
+
+if attempt_count> -1:
+	print()
+	print("Word: "+ ''.join(answered))
+	print()
+	print("Сongratulations, you guessed this word)")
+	print()
+else:
+	print("You loose this game(")
